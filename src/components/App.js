bd97eefd45ff8e4833b2d80c1766546e6c7e97f4
Vidya <vidyasba@gmail.com>
2024-02-13 17:01:46 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,49 @@
-
-import React ,{useState} from "react";
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  const[todo, setTodo]=useState('');
-  const[todos ,setTodos] = useState([]);
-  const handleAdd=e=>{
-    e.preventDefault();
-    setTodos([...todos,todo]);
-    setTodo('');
-    console.log(todos);
-  }
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const handleDelete=(index)=>{
-    setTodos(todos.filter((todo,i)=> i!==index));
-  }
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (todo.trim() !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>To-Do List</h1>
-        <form>
-          <input type="text" id="todo" value={todo} 
-          onChange={(e)=>setTodo(e.target.value)}name="todo"/>
-          <button onClick={handelAdd}>Add Todo</button>
-        </form>
+      <h1>To-Do List</h1>
+      <form onSubmit={handleAdd}>
+        <input
+          type="text"
+          id="todo"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          name="todo"
+          placeholder="Enter a task..."
+        />
+        <button type="submit">Add Todo</button>
+      </form>
 
-        <ul>
-         {todos.map((todo,index)=>{
-          return(
-            <li key={index}>
-              <div className="todo-item">
-                <p>{todo}</p>
-                <button onClick={()=>{handleDelete}}>Delete</button>
-              </div>
-            </li>
-          )
-         })}
-        </ul>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <div className="todo-item">
+              <p>{todo}</p>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
